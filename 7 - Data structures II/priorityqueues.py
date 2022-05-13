e28119334f19d873b3803c52165a21ce94ba6aa2
David Miles-Skov <davidmilesskov@protonmail.com>
2022-05-13 19:40:59 +0200

    Update priorityqueues.py

--- 7 - Data structures II/priorityqueues.py
+++ 7 - Data structures II/priorityqueues.py
@@ -1,36 +1,101 @@
-class Node:
-    def __init__(self,i, difficulty):
-        self.i = i
-        self.difficulty = difficulty
-    def set_next(self, next):
-        self.next = next
-    
-    def get_next(self):
-        return self.next
-    
-    def get_diff(self):
-        return self.difficulty
+from math import floor
+from re import L
 
-class TaskDelegation:
+class PriorityQueue:
     
-    def __init__(self,n):
-        self.top = n
+    def __init__(self, array=[]):
+        self.array = [None]+array
     
-    def new_task(self,n):
-        curr_node = self.top
-        prev_node = None
-        while curr_node.get_diff()>n.get_diff():
-            prev_node = curr_node
-            if curr_node.get_next() == None:
-                curr_node.set_next(n)
-                return
+    def bubbleUp(self, x, idx_x):
+        parent = self.array[floor(idx_x/2)]
+        if parent!=None:
+            if parent < x:
+                # swapping x and parent
+                idx_parent = floor(idx_x/2)
+                self.array[idx_parent]=x
+                self.array[idx_x]=parent
+                return self.bubbleUp(x, idx_parent)
             else:
-                curr_node = curr_node.get_next()
-        prev_node.set_next(n)
-        n.set_next(curr_node)
-    
-    def request_task(self):
-        r = self.top
-        self.top = self.top.get_next()
-        return r
+                return self.array
+        else:
+            return self.array
+        
+    def bubbleDown(self,x,idx_x):
+        idx_l = idx_x*2
+        idx_r = idx_x*2+1
+        # Checking if right out of bounds
+        if idx_r > len(self.array)-1:
+            # check if left is out of bounds
+            if idx_l>len(self.array)-1:
+                return self.array
+            # left is not out of bounds
+            else:
+                left = self.array[idx_l]
+                # if left is greater than x, swap left and x and terminate
+                if left>x:
+                    self.array[idx_x]=left
+                    self.array[idx_l]=x
+                return self.array
+        # right is not out of bounds
+        else:
+            # check if smaller than left or right
+            left = self.array[idx_l]
+            right = self.array[idx_r]
+            if left > x or right > x:
+                m = max([left, right])
+                if m==left:
+                    #swap left with x
+                    self.array[idx_x]=left
+                    self.array[idx_l]=x
+                    self.bubbleDown(x, idx_l)
+                if m==right:
+                    #swap right with x
+                    self.array[idx_x]=right
+                    self.array[idx_r]=x
+                    self.bubbleDown(x, idx_r)
 
+    def max(self):
+        return self.array[1]
+    
+    def insert(self,x):
+        self.array.append(x)
+        self.array = self.bubbleUp(x,len(self.array)-1)
+    
+    def extractMax(self):
+        if len(self.array)==1:
+            return 
+        elif len(self.array)==2:
+            x=self.array[1]
+            self.array=[None]
+            return x
+        else:
+            x = self.array[1]
+            self.array=[None]+self.array[2::]
+            self.bubbleDown(self.array[1],1)
+            return x
+    def __str__(self):
+        if self.array==[None]:
+            return ""
+        else:
+            s = ""
+            for i in self.array[1::]:
+                s+=str(i)+" "
+        return s
+    def __repr__(self):
+        return self.__str__()
+
+
+def main():
+    p = PriorityQueue()
+    N = int(input())
+    for i in range(N):
+        s = input()
+        if s.split()[0]=="I":
+            p.insert(int(s.split()[1]))
+        if s=="P":
+            print(p)
+        if s=="E":
+            print(p.extractMax())
+
+main()
+    
